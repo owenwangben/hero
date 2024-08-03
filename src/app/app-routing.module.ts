13337fd8c552acd2d691028bd6c3d67bc03142d3
owen <try09067888@gmail.com>
2024-08-03 15:46:10 +0800

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,19 @@
-import { HeroesComponent } from './heroes/heroes.component';
-import { RouterModule , Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-
-const routes:Routes = [
-  {path : '' , redirectTo:'/dashboard',pathMatch:'full'},
-  {path : 'heroes',component:HeroesComponent},
-  {path : 'dashboard',component:DashboardComponent},
-  {path : 'detail/:id',component:HeroDetailComponent},
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent }
 ];
-@NgModule({
-  declarations: [],
-  imports: [CommonModule , RouterModule.forRoot(routes)],
-  exports:[RouterModule]
-})
 
-export class AppRoutingModule { }
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,25 +1,30 @@
-import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
-createDb(){
-  const heroes = [
-    { id: 12, name: 'Dr. Nice' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr. IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-  ];
-  return {heroes};
-}
+export class InMemoryDataService implements InMemoryDbService {
+
+  createDb(){
+    const heroes = [
+      { id : 11 , name : 'owen'},
+      { id : 12 , name : 'vanessa'},
+      { id : 13 , name : 'kelly'},
+      { id : 14 , name : 'paul'},
+      { id : 15 , name : 'eden'},
+      { id : 16 , name : 'mia'},
+      { id : 17 , name : 'john'},
+      { id : 18 , name : 'potter'},
+      { id : 19 , name : 'wilson'},
+    ];
+    return {heroes};
+  }
+
+  getId(heroes:Hero[]):number{
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id))+1:11;
+  }
+
   constructor() { }
 }
